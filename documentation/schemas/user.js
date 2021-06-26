f18ca669df1bd14faba512ccedb0700080c51cfa
Jhon Velasquez <jhon.velasquez@wolox.co>
2021-06-26 13:23:24 -0500

    feat: It was cretated the documentation for the endpoint POST /users

--- documentation/schemas/user.js
+++ documentation/schemas/user.js
@@ -1,38 +1,86 @@
+const { USER_ALREADY_EXIST, USER_CREATED } = require('../../config/constants');
+const { DUPLICATED_REGISTER_ERROR, BAD_REQUEST } = require('../../app/errors');
+
 module.exports = {
-  userId: {
-    type: 'integer',
-    example: 7
-  },
-  username: {
-    type: 'string',
-    example: 'tom99'
-  },
-  userEmail: {
-    type: 'string',
-    example: 'tom.engels@wolox.com.ar'
-  },
-  User: {
+  userInput: {
     type: 'object',
     properties: {
-      id: {
-        $ref: '#/components/schemas/userId'
+      name: {
+        type: 'string',
+        example: 'John'
       },
-      username: {
-        $ref: '#/components/schemas/username'
+      lastName: {
+        type: 'string',
+        example: 'Doe'
       },
-      email: {
-        $ref: '#/components/schemas/userEmail'
+      mail: {
+        type: 'string',
+        example: 'john.doe@wolox.com.ar'
+      },
+      password: {
+        type: 'string',
+        example: 'MyP4ssWor%'
       }
     }
   },
-  Users: {
+  userCreated: {
     type: 'object',
     properties: {
-      users: {
-        type: 'array',
-        items: {
-          $ref: '#/components/schemas/User'
+      message: {
+        type: 'string',
+        example: USER_CREATED
+      },
+      data: {
+        type: 'object',
+        properties: {
+          name: {
+            type: 'string',
+            example: 'John'
+          }
         }
+      }
+    }
+  },
+  alreadyExist: {
+    type: 'object',
+    properties: {
+      message: {
+        type: 'string',
+        example: USER_ALREADY_EXIST
+      },
+      internal_code: {
+        type: 'string',
+        example: DUPLICATED_REGISTER_ERROR
+      }
+    }
+  },
+  userInvalidParameter: {
+    type: 'object',
+    properties: {
+      message: {
+        type: 'object',
+        properties: {
+          value: {
+            type: 'string',
+            example: 4
+          },
+          msg: {
+            type: 'string',
+            example: "'name' must be a string"
+          },
+          param: {
+            type: 'string',
+            example: 'name'
+          },
+          location: {
+            type: 'string',
+            example: 'body'
+          }
+        }
+      },
+      internal_code: {
+        type: 'string',
+        example: BAD_REQUEST
       }
     }
   }
